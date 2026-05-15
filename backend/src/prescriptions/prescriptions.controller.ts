204719b45587ce8f846a0e43f8a750c7f3e1641a
Samuel Developer <80781737+samueldeve6@users.noreply.github.com>
2026-05-14 19:11:09 -0500

    roles

--- backend/src/prescriptions/prescriptions.controller.ts
+++ backend/src/prescriptions/prescriptions.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Req } from '@nestjs/common';
+import { PrescriptionsService } from './prescriptions.service.js';
+import { CreatePrescriptionDto } from './dto/create-prescription.dto.js';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard.js';
+import { Roles } from '../common/decorators/roles.decorator.js';
+// Nota: Necesitarás un RolesGuard para que @Roles funcione, 
+// pero por ahora usemos el Guard de JWT para validar la conexión.
 
 @Controller('prescriptions')
-export class PrescriptionsController {}
+@UseGuards(JwtAuthGuard)
+export class PrescriptionsController {
+    constructor(private readonly prescriptionsService: PrescriptionsService) {}
+
+    @Post()
+    @Roles('doctor') // Solo los doctores pueden crear recetas
+    create(@Body() createPrescriptionDto: CreatePrescriptionDto, @Req() req) {
+    // El 'sub' es el ID que viene en el token que ya probamos
+    const userId = req.user.userId || req.user.sub;
+    
+    return this.prescriptionsService.create(createPrescriptionDto, userId);
+    }
+}
